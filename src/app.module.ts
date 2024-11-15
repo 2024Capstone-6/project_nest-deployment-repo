4db8e8e3b5f145acec87ecf783bb108a95d0a712
Soyeon Lim <156628006+dlatth@users.noreply.github.com>
2024-11-15 18:51:25 +0900

    ✨ feat: app.module.ts 기본 설정 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+// 모듈 import
 import { JapanModule } from './japan/japan.module';
 
+// 엔티티 import
+import { Activities } from './japan/entities/activities/activities.entity';
+
 @Module({
-  imports: [JapanModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // .env 파일을 불러오기 위한 설정
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10), // 10진수로 변환
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [Activities], // 엔티티를 수동으로 로드
+      // 본인 엔티티 import 후 넣어서 작업하세요
+      synchronize: true, // 데이터베이스 스키마 동기화
+    }),
+    JapanModule, // 모듈 import
+  ],
 })
 export class AppModule {}
