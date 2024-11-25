59463b7c652ecca18df4340ad11b8a2eed28d2c3
Soyeon Lim <156628006+dlatth@users.noreply.github.com>
2024-11-25 15:32:03 +0900

    🐛 fix : Update CORS problem from main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // CORS 설정
   app.enableCors({
     origin: 'http://localhost:5173', // 프론트엔드 도메인만 허용
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
