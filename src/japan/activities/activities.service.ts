eead7c1b3950de36cfbb916594cb85a8e9dc785c
Soyeon Lim <156628006+dlatth@users.noreply.github.com>
2024-12-09 15:20:47 +0900

    ✨edit: 주석 수정

--- src/japan/activities/activities.service.ts
+++ src/japan/activities/activities.service.ts
@@ -1,42 +1,30 @@
-// 이 클래스가 NestJS의 의존성 주입 시스템에 의해 관리 될 수 있음을 나타냄
 import { Injectable } from '@nestjs/common';
-// TypeORM 리포지토리를 주입하는 데 사용됨
 import { InjectRepository } from '@nestjs/typeorm';
-// 데이터베이스 작업을 수행하는 데 사용됨
 import { Repository } from 'typeorm';
-// 데이터베이스 테이블과 매핑
 import { Activities } from '../entities/activities.entity';
-// DTO 클래스를 주입하는 데 사용됨
 import { CreateActivityDto } from './dto/create-activity.dto';
 import { UpdateActivityDto } from './dto/update-activity.dto';
 
 @Injectable()
-// 클래스를 정의, 활동과 관련된 비즈니스 로직을 처리
 export class ActivitiesService {
   constructor(
-    // TypeORM 리포지토리를 주입하는 데 사용됨
     @InjectRepository(Activities)
     private activitiesRepository: Repository<Activities>,
-    // 생성자에서 Activities 엔티티에 대한 리포지토리를 주입
   ) {}
 
   // CREATE: 새로운 활동 게시물을 생성하는 메서드
   async createActivity(
-    // 생성할 활동 게시물의 데이터를 받아옴
     createActivityDto: CreateActivityDto,
   ): Promise<Activities> {
-    // 생성할 활동 게시물의 데이터를 사용하여 새로운 엔티티 생성
     const activity = this.activitiesRepository.create({
       ...createActivityDto,
       date: new Date().toISOString().split('T')[0], // 현재 날짜 자동 설정
     });
-    // 생성한 엔티티를 데이터베이스에 저장
     return this.activitiesRepository.save(activity);
   }
 
   // READ: 모든 활동 게시물을 찾는 메서드 (테스트용)
   async findAllActivities(): Promise<Activities[]> {
-    // 모든 활동 엔티티를 찾아 반환
     return this.activitiesRepository.find();
   }
 
@@ -45,28 +33,23 @@ export class ActivitiesService {
     page: number,
     limit: number,
   ): Promise<{ items: Activities[]; total: number }> {
-    // 페이지네이션을 위한 오프셋 계산
     const offset = (page - 1) * limit;
-    // 데이터베이스에서 페이지네이션을 위한 데이터 조회
     const [items, total] = await this.activitiesRepository.findAndCount({
-      skip: offset, // 오프셋을 설정하여 특정 위치부터 데이터를 가져옴
-      take: limit, // 한번에 가져올 데이터의 수를 설정
+      skip: offset,
+      take: limit,
     });
 
     return { items, total };
   }
 
-  // READ: 특정 활동 게시물을 찾는 메서드 (클릭 시 게시물 전체를 보여줌)
+  // READ: 특정 활동 게시물을 찾는 메서드
   async findActivityById(id: number): Promise<Activities> {
     return this.activitiesRepository.findOneBy({ id });
   }
 
-  // READ: 이메일이나 제목으로 활동 게시물을 찾는 검색 메소드 개발 예정
-
   // UPDATE: 활동 게시물을 업데이트하는 메서드
   async updateActivity(
     id: number,
-    // 업데이트할 활동 게시물의 데이터를 받아옴
     updateActivityDto: UpdateActivityDto,
   ): Promise<Activities> {
     await this.activitiesRepository.update(id, updateActivityDto);
@@ -75,7 +58,6 @@ export class ActivitiesService {
 
   // DELETE: 활동 게시물을 삭제하는 메서드
   async removeActivity(id: number): Promise<void> {
-    // 주어진 ID를 사용하여 엔티티를 삭제
     await this.activitiesRepository.delete(id);
   }
 }
--- src/japan/japanese/japanese.service.ts
+++ src/japan/japanese/japanese.service.ts
@@ -13,7 +13,7 @@ export class JapaneseService {
     private japaneseRepository: Repository<Japanese>,
   ) {}
 
-  // CREATE
+  // CREATE: 새로운 일본어 게시물을 생성하는 메서드
   async createJapanese(
     createJapaneseDto: CreateJapaneseDto,
   ): Promise<Japanese> {
@@ -24,12 +24,12 @@ export class JapaneseService {
     return this.japaneseRepository.save(japanese);
   }
 
-  // READ
+  // READ: 모든 일본어 게시물을 찾는 메서드
   async findAllJapanese(): Promise<Japanese[]> {
     return this.japaneseRepository.find();
   }
 
-  // READ - 페이지네이션 일본어 게시물 가져오기
+  // READ: 페이지네이션을 위한 일본어 게시물을 찾는 메서드
   async findJapaneseWithPagination(
     page: number,
     limit: number,
@@ -44,9 +44,9 @@ export class JapaneseService {
     return { items, total };
   }
 
-  // READ - 이메일이나 제목으로 일본어 게시물 검색 (개발 예정)
+  // READ: 이메일이나 제목으로 일본어 게시물 검색 (개발 예정)
 
-  // READ - 특정 ID의 게시물 찾기
+  // READ: 특정 ID의 게시물 찾기
   async findOne(id: number): Promise<Japanese> {
     const japanese = await this.japaneseRepository.findOneBy({ id });
     if (!japanese) {
@@ -55,7 +55,7 @@ export class JapaneseService {
     return japanese;
   }
 
-  // UPDATE
+  // UPDATE: 일본어 게시물을 업데이트하는 메서드
   async updateJapanese(
     id: number,
     updateJapaneseDto: UpdateJapaneseDto,
@@ -64,7 +64,7 @@ export class JapaneseService {
     return this.japaneseRepository.findOneBy({ id });
   }
 
-  // DELETE
+  // DELETE: 일본어 게시물을 삭제하는 메서드
   async removeJapanese(id: number): Promise<void> {
     await this.japaneseRepository.delete(id);
   }
