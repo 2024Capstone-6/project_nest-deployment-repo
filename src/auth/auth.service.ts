dbf7f44c94a4211c7f2fa2b331d4fe71f67d87f0
Soyeon Lim <156628006+dlatth@users.noreply.github.com>
2024-12-13 17:50:31 +0900

    ✨edit: userdt unusable code delete

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,25 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { UserdtService } from '../userdt/userdt.service';
 
 @Injectable() // 서비스를 의존성 주입이 가능한 상태로 만듬
 export class AuthService {
   constructor(
     // 객체가 생성될 때 초기화시켜주는 함수
-    private readonly userdtService: UserdtService,
     private readonly jwtService: JwtService,
-    // AuthService의 생성자에서 UserdtService와 JwtService를 주입받아 사용
   ) {}
-
-  // 사용자의 이메일과 비밀번호를 확인하여 유효성을 검증하는 메서드
-  async validateUser(email: string, password: string): Promise<any> {
-    const user = await this.userdtService.validateUser(email, password); // UserdtService의 validateUser 메서드를 호출하여 이메일과 비밀번호가 맞는 사용자를 찾기
-    if (user) {
-      const { ...result } = user;
-      return result;
-    }
-    return null; // 사용자가 유효하지 않을 경우 null을 반환
-  }
 
   // 사용자가 로그인 요청을 하면 호출되는 메서드
   async login(user: any) {
